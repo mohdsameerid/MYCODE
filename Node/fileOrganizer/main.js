68bdaccf8055c60b0d111c8af13493633c77da50
mohdsameerid <mohdsameeredu@gmail.com>
2022-08-14 13:58:48 +0530

    main.js change

--- Node/fileOrganizer/main.js
+++ Node/fileOrganizer/main.js
@@ -1,25 +1,27 @@
-let inputArr = process.argv.slice(2);
-// console.log(inputArr)
-let cmd = inputArr[0];
-let path = inputArr[1];
-// let helpFunc = require("./commands/help");
+// entry point of my command line 
 let helpFunc = require("./commands/help");
-// console.log(cmd);
-// console.log(path)
-console.log(helpFunc);
-switch (cmd) {
-    case "help":
-        console.log("help");
-        helpFunc.help();
+
+let inputArr = process.argv.slice(2);
+// console.log(inputArr);
+let command = inputArr[0];
+let path = inputArr[1];
+switch (command) { //organizee
+    case "tree":
+        //call tree function
+        treeFunc.tree(path);
+        // console.log("tree function called and executed succesfully on path " + path);
         break;
     case "organize":
-        // console.log("organize");
-
-        break;    
-    case "tree":
-        console.log("tree");
-         break;
+        //call organize function
+        orgFunc.organize(path);
+        // console.log("organize function called and executed succesfully on path "+ path);
+        break;
+    case "help":
+        //call help function
+        helpFunc.help();
+        // console.log("help function called and executed succesfully");
+        break;
     default:
-        console.log("Entered Incorrect cmd");
+        console.log("command not recognized :/")
         break;
 }
