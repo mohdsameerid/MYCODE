79d3b360d9305ae3da5201ca7c4798d95d205ef9
mohdsameerid <mohdsameeredu@gmail.com>
2022-08-17 00:03:20 +0530

    added readme file

--- Node/fileOrganizer/commands/organize.js
+++ Node/fileOrganizer/commands/organize.js
@@ -1,6 +1,5 @@
-
-const fs = require("fs");     // fs module
-const path = require("path"); // path module
+const fs = require("fs"); //fs module
+const path = require("path"); //path module
 let types = {
     media: ["mp4", "mkv", "mp3","mov"],
     archives: ['zip', '7z', 'rar', 'tar', 'gz', 'ar', 'iso', "xz"],
@@ -9,22 +8,26 @@ let types = {
     images: ['png','jpg','jpeg']
 }
 
-function organize(srcPath){
-    if(srcPath == undefined){
-        // console.log(srcPath);
-        //The process.cwd() method returns the current working directory of the Node.js process.
-        srcPath = process.cwd();
-        // console.log("current srcPath :" +srcPath);
-    }
 
-    let organizedFiles = path.join(srcPath, "organized_files");
-    // console.log("organized file path: "+organizedFiles);
-    if(!fs.existsSync(organizedFiles)){
-        fs.mkdirSync(organizedFiles)
-    }
-    else{
-        console.log("organizedFiles dir already exixts");
-    }
+function organize(srcPath) {
+  //1. to check if srcPath is present
+  if (srcPath == undefined) {
+    //The process.cwd() method returns the current working directory of the Node.js process.
+    //console.log(srcPath); //undefined
+    srcPath = process.cwd();
+    // console.log("source path is ",srcPath);
+  }
+
+  //2. to create a directory-> organized_files
+  // let organizedFiles = srcPath + "/" + "organized_files";
+  let organizedFiles = path.join(srcPath, "organized_files");
+  console.log("organized files folder path is ", organizedFiles);
+  if (fs.existsSync(organizedFiles) == false) {
+    //organizedfiles naam ka folder exist nhi krta to ek folder bana do warna rhne do
+    fs.mkdirSync(organizedFiles);
+  } else console.log("folder already exists");
 }
 
-organize();
+//  path       C:\Users\Dell\Documents\Samir Code\Mycode\Node\fileOrganizer\downloads
+let srcPath = "C:\Users\Dell\Documents\Samir Code\Mycode\Node\fileOrganizer\downloads";
+organize(srcPath);
